bf71c5cbef65184610365e5ace952e2d1d32effe
VelinIliev <veliniliev@gmail.com>
2023-09-19 14:04:34 +0300

    Data Aggregation - Lab

--- 07-DataAggregation-Lab.sql
+++ 07-DataAggregation-Lab.sql
@@ -36,21 +36,6 @@ HAVING SUM(salary) < 4200
 ORDER BY department_id;
 
 -- 08. Department Names
--- Write a query to retrieve information from table employees about the department names,
--- according to department_id. The output should have the following fields:
--- •	id of the employee
--- •	first_name
--- •	last_name
--- •	salary – formatted to the second decimal place.
--- •	department_id
--- •	department_name – use Simple CASE Expression
--- o	1 – "Management"
--- o	2 – "Kitchen Staff"
--- o	3 – "Service Staff"
--- o	any other number – "Other"
--- See the examples for more information. Format the salary to 2 digits after the decimal point.
--- Submit your queries, ordered by employee id.
-
 SELECT
     e.id,
     e.first_name,
