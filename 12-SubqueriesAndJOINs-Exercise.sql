731c245c1ab2824a84c92a96fffe66be63527a65
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2023-09-30 22:15:47 +0300

    Subqueries and JOINs - Exercise

--- 12-SubqueriesAndJOINs-Exercise.sql
+++ 12-SubqueriesAndJOINs-Exercise.sql
@@ -228,26 +228,28 @@ ORDER BY currency_usage DESC;
 -- 20. The Highest Peak in Each Country
 
 WITH row_number AS (
-    SELECT
-        coun.country_name AS "Country",
-        COALESCE(p.peak_name, '(no highest peak)') AS "Highest Peak Name",
-        COALESCE(p.elevation, 0) AS "Highest Peak Elevation",
-        COALESCE(m.mountain_range, '(no mountain)') AS "Mountain",
-        ROW_NUMBER ()
-            OVER (
-                PARTITION BY country_name
-                ORDER BY p.elevation DESC
-            ) AS "rank"
-    FROM countries AS coun
-    LEFT JOIN mountains_countries mc on coun.country_code = mc.country_code
+  SELECT
+    c.country_name,
+    p.peak_name,
+    p.elevation,
+    m.mountain_range,
+    ROW_NUMBER() OVER (
+        PARTITION BY c.country_name
+        ORDER BY p.elevation DESC
+        ) AS peak_rank
+  FROM countries c
+    LEFT JOIN mountains_countries mc on c.country_code = mc.country_code
     LEFT JOIN mountains m on mc.mountain_id = m.id
     LEFT JOIN peaks p on m.id = p.mountain_id
-    ORDER BY "Country", "Highest Peak Elevation" DESC
 )
 SELECT
-    "Country",
-    "Highest Peak Name",
-    "Highest Peak Elevation",
-    "Mountain"
+    country_name,
+    COALESCE(peak_name, '(no highest peak)') AS peak_name,
+    COALESCE(elevation, 0) AS elevation,
+    CASE
+        WHEN peak_name IS NULL OR mountain_range IS NULL THEN '(no mountain)'
+        ELSE mountain_range
+    END AS mountain
 FROM row_number
-WHERE "rank" = 1;
+WHERE peak_rank = 1
+ORDER BY country_name, elevation DESC;
